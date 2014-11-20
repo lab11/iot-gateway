439093d26348c4e7226a3640da1a0b20ebcf3065
Thomas Zachariah <tzachari@umich.edu>
2014-11-20 13:32:34 -0500

    Added missing package

--- app/Gateway/app/src/main/java/edu/umich/eecs/lab11/gateway/Gateway.java
+++ app/Gateway/app/src/main/java/edu/umich/eecs/lab11/gateway/Gateway.java
@@ -12,6 +12,7 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Handler;
+import android.provider.Settings;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
